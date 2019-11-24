531a7aab25b0bcbf75028b396c64dca4bb73e7e2
hlzhou <zhouhl.hust@gmail.com>
2019-11-23 23:26:01 -0600

    working version of bottom navigation, not displaying icons and showing previous theme when click

--- ExploreUTXPlatform/layouts/BottomNavigator.js
+++ ExploreUTXPlatform/layouts/BottomNavigator.js
@@ -1,39 +1,38 @@
 import React, {Component} from 'react';
 import { BottomNavigation, Text } from 'react-native-paper';
-import {Icon} from 'react-native-vector-icons/FontAwesome5';
 
 
-const BuildingRoute = () => <Text >Building</Text>;
-
-const StudyRoute = () => <Text>Study</Text>;
-
-const ActivityRoute = () => <Text>Activity</Text>;
-
-const StatueRoute = () => <Text>Statue</Text>;
-
+// currently not under use by any screen.
 class BottomNavigator extends Component {
 
   constructor() {
     super();
     this.state = {
-    index: 0,
-    routes: [
-      { key: 'building', title: 'Building', icon: 'building' },
-      { key: 'study', title: 'Study', icon: 'library' },
-      { key: 'activity', title: 'Activity', icon: 'activity' },
-      { key: 'statue', title: 'Statue', icon: 'street_view' },
-    ],
-  };
+        index: 0,
+        routes: [
+        { key: 'building', title: 'Building', icon: 'building' },
+        { key: 'study', title: 'Study', icon: 'library' },
+        { key: 'activity', title: 'Activity', icon: 'activity' },
+        { key: 'statue', title: 'Statue', icon: 'street_view' },
+        ],
+    };
   }
-  
+
+    BuildingRoute = () => <Text>Building</Text>;
+
+    StudyRoute = () => <Text>Study</Text>;
+
+    ActivityRoute = () => <Text>Activity</Text>;
+
+    StatueRoute = () => <Text>Statue</Text>;
 
   handleIndexChange = index => this.setState({ index });
 
   renderScene = BottomNavigation.SceneMap({
-    building: BuildingRoute,
-    study: StudyRoute,
-    activity: ActivityRoute,
-    statue: StatueRoute,
+    building: this.BuildingRoute,
+    study: this.StudyRoute,
+    activity: this.ActivityRoute,
+    statue: this.StatueRoute,
   });
 
   render() {
@@ -42,6 +41,7 @@ class BottomNavigator extends Component {
         navigationState={this.state}
         onIndexChange={this.handleIndexChange}
         renderScene={this.renderScene}
+        barStyle={{backgroundColor:'#BF5700'}}
       />
     );
   }
