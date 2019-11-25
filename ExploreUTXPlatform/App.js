93fb695e6753193d38ee09617300c9f7d6733ab7
Ruei-Bang Chen <rebang100@gmail.com>
2019-11-25 12:49:33 -0600

    Adjust the color, titles, and icons, of the drawer navigation

--- ExploreUTXPlatform/App.js
+++ ExploreUTXPlatform/App.js
@@ -1,6 +1,9 @@
+import React, {Component} from 'react';
+import {View} from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 import Icon from "react-native-vector-icons/Ionicons";
+
 import ViewAllScreen from './screens/ViewAllScreen';
 import ManageScreen from './screens/ManageScreen';
 import MapScreen from './screens/MapScreen';
@@ -8,18 +11,53 @@ import CreateNewPlaceScreen from './screens/CreateNewPlaceScreen';
 
 const MyDrawerNavigator = createDrawerNavigator({
   ViewAll: {
+    navigationOptions: {
+      drawerIcon: ({ tintColor }) => (
+          <Icon name="md-photos" style={{ color: tintColor }} />
+      ),
+      drawerLabel: "View All Places",
+
+    },
     screen: ViewAllScreen, 
   },
   Map: {
+    navigationOptions: {
+      drawerIcon: ({ tintColor }) => (
+          <Icon name="md-map" style={{ color: tintColor }} />
+      ),
+      drawerLabel: "Map"
+    },
     screen: MapScreen,
   },
   CreateNewPlace:{
+    navigationOptions: {
+      drawerIcon: ({ tintColor }) => (
+          <Icon name="md-camera" style={{ color: tintColor }} />
+      ),
+      drawerLabel: "Add New Places"
+    },
     screen: CreateNewPlaceScreen,
   },
   Manage: {
+    navigationOptions: {
+      drawerIcon: ({ tintColor }) => (
+          <Icon name="md-build" style={{ color: tintColor }} />
+      ),
+      drawerLabel: "Manage"
+    },
   	screen: ManageScreen,
   },
 }, {
+  contentOptions: {
+    activeTintColor: '#BF5700',
+    activeBackgroundColor: '#E2E2E2',
+    inactiveTintColor: 'black',
+    inactiveBackgroundColor: 'transparent',
+    labelStyle: {
+      fontSize: 15,
+      marginLeft: 10,
+    },
+  },
   drawerPosition: 'left',
   // contentComponent: CustomDrawerNavigation,
   drawerOpenRoute: 'DrawerOpen',
