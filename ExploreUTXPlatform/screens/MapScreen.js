5b34e9d9a9dd112690a9f2ab07cda162e27b51ef
hlzhou <zhouhl.hust@gmail.com>
2019-11-25 13:18:38 -0600

    cleaned MapScreen.js

--- ExploreUTXPlatform/screens/MapScreen.js
+++ ExploreUTXPlatform/screens/MapScreen.js
@@ -90,11 +90,11 @@ class MapScreen extends Component {
     this.updateMap(index);
   };
 
-  async updateMap (index) {
+  updateMap (index) {
     if (index === 0) {
-      await this.getPlaces();
+      this.getPlaces();
     } else {
-      await this.getThemePlaces(themes[index]);
+      this.getThemePlaces(themes[index]);
     }
   }
 
@@ -228,7 +228,7 @@ class MapScreen extends Component {
       this.GetLocation();
       this.WatchLocation();
     }
-    await this.getPlaces();
+    this.getPlaces();
   }
 
   onRegionChangeComplete = region => {
